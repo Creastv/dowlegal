5ded3249d8835524a2579d1d30010b8e78222db3
Creastv <creastv@gail.com>
2025-03-21 14:28:55 +0100

    title-page mobile

--- footer.php
+++ footer.php
@@ -39,17 +39,6 @@
     </svg>
 </span>
 <?php wp_footer(); ?>
-<script>
-    if ($(window).width() > 500) {
-        jQuery('.go-parallex').paroller({
-            factor: -0.3, // multiplier for scrolling speed and offset, +- values for direction control  
-            // factorLg: 0.4, // multiplier for scrolling speed and offset if window width is less than 1200px, +- values for direction control  
-            type: 'foreground', // background, foreground  
-            direction: 'vertical', // vertical, horizontal  
-            transition: 'translate 0.1s ease' // CSS transition, added on elements where type:'foreground' 
-        });
-    }
-</script>
 </body>
 
 </html>
--- src/js/go-main.js
+++ src/js/go-main.js
@@ -68,15 +68,12 @@ for (let i = 0; i < calaps.length; i++) {
 }
 
 
-
-  // document.querySelector(".js-navbar__toggler").addEventListener("click", function() {
-  //   document.getElementById("toggleMenu").click();
-  // });
-
-  // document.querySelectorAll("img").forEach(img => {
-  //   if (!img.hasAttribute("width") || !img.hasAttribute("height")) {
-  //     img.setAttribute("width", img.naturalWidth);
-  //     img.setAttribute("height", img.naturalHeight);
-  //   }
-  // });
-  
+if (jQuery(window).width() > 500) {
+  jQuery('.go-parallex').paroller({
+      factor: -0.3, // multiplier for scrolling speed and offset, +- values for direction control  
+      // factorLg: 0.4, // multiplier for scrolling speed and offset if window width is less than 1200px, +- values for direction control  
+      type: 'foreground', // background, foreground  
+      direction: 'vertical', // vertical, horizontal  
+      transition: 'translate 0.1s ease' // CSS transition, added on elements where type:'foreground' 
+  });
+}
