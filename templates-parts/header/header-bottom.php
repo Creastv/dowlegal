1d9b170b90643986ac94bc730e56618ecc9c5108
Creastv <creastv@gail.com>
2025-03-21 15:06:44 +0100

    kalk

--- templates-parts/header/header-bottom.php
+++ templates-parts/header/header-bottom.php
@@ -1,8 +1,8 @@
 <?php
-$btn_one_link = get_theme_mod('header_btn_one_link', '');
-$btn_one_text = get_theme_mod('header_btn_one_text', '');
-$btn_two_link = get_theme_mod('header_btn_two_link', '');
-$btn_two_text = get_theme_mod('header_btn_two_text', '');
+$btn_one_link = get_theme_mod('header_phone_number', '');
+$btn_one_link = get_theme_mod('header_phone_number', '');
+$btn_one_link = get_theme_mod('header_phone_number', '');
+$btn_one_link = get_theme_mod('header_phone_number', '');
 ?>
 <div class="h-nav">
     <div class="container-full">
@@ -18,19 +18,30 @@ $btn_two_text = get_theme_mod('header_btn_two_text', '');
                     'echo'               => false,
                     'items_wrap'       => '<div class="navbar__navigation__list"><ul id="%1$s" class="%2$s js-navbar__navigation__list " itemscope itemtype="https://www.schema.org/SiteNavigationElement">%3$s </ul></div>',
                 ));
+                // $temp_menu = str_replace("<a", "<a itemprop='url' ", $temp_menu); // We set an attribute for menu items through replacement
+
+                // preg_match_all("~<a (.*?)>(.*)</a>~", $temp_menu, $matchesz); // Find all the "a" tags through the regular expression to search for the "span" tag
+                // foreach ($matchesz[0] as $value) {
+                //     // We are looking for a nested tag "span" in the current tag "a"
+                //     if (strpos($value, "<span") === false) {  // If the current "a" tag doesn't have a "span" nested tag, use a regular expression to wrap the content of the "a" tag in a "span" and set the markup attribute to it
+                //         $temp_value = preg_replace("~<a (.*?)>(.*)</a>~", "<a $1><span itemprop='name'>$2</span></a>", $value);
+
+                //         $temp_menu = str_replace($value, $temp_value, $temp_menu);
+                //     } else { // If the "span" tag is found, replace it with a markup attribute
+                //         $temp_value = str_replace("<span", "<span itemprop='name'", $value);
+
+                //         $temp_menu = str_replace($value, $temp_value, $temp_menu);
+                //     }
+                // }
                 echo $temp_menu;
+
                 ?>
             </nav>
-
             <div class="h-nav__buttons">
-                <?php if (!empty($btn_one_link)) : ?>
-                    <a href="<?php echo $btn_one_link; ?>" class="btn-rev"><?php echo $btn_one_text; ?></a>
-                <?php endif; ?>
-                <?php if (!empty($btn_two_link)) : ?>
-                    <a href="<?php echo $btn_two_link; ?>" class="btn-main"><?php echo $btn_two_text; ?></a>
-                <?php endif; ?>
+                <a href="https://dowlegal.jffrbblxkx.cfolks.pl/kalkulator-sankcji.html" class="btn-rev">Kalkulator
+                    sankcji</a>
+                <a href="#" class="btn-main">Bezpłatna analiza</a>
             </div>
-
         </div>
     </div>
 </div>
