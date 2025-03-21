1c0ef827d0c42af0cbf459ab4da4e93867e4c270
Creastv <creastv@gail.com>
2025-03-21 14:24:38 +0100

    title-page mobile

--- footer.php
+++ footer.php
@@ -40,13 +40,15 @@
 </span>
 <?php wp_footer(); ?>
 <script>
-    jQuery('.go-parallex').paroller({
-        factor: -0.3, // multiplier for scrolling speed and offset, +- values for direction control  
-        // factorLg: 0.4, // multiplier for scrolling speed and offset if window width is less than 1200px, +- values for direction control  
-        type: 'foreground', // background, foreground  
-        direction: 'vertical', // vertical, horizontal  
-        transition: 'translate 0.1s ease' // CSS transition, added on elements where type:'foreground' 
-    });
+    if ($(window).width() > 500) {
+        jQuery('.go-parallex').paroller({
+            factor: -0.3, // multiplier for scrolling speed and offset, +- values for direction control  
+            // factorLg: 0.4, // multiplier for scrolling speed and offset if window width is less than 1200px, +- values for direction control  
+            type: 'foreground', // background, foreground  
+            direction: 'vertical', // vertical, horizontal  
+            transition: 'translate 0.1s ease' // CSS transition, added on elements where type:'foreground' 
+        });
+    }
 </script>
 </body>
 
