92c6a899343b637d14cdfc0c6b429c2d44342c6c
Creastv <creastv@gail.com>
2025-03-26 15:33:55 +0100

    fadein

--- blocks/bullets/bullets.php
+++ blocks/bullets/bullets.php
@@ -9,25 +9,30 @@ if ($col == 2) {
 <?php if (have_rows('bullets')): ?>
     <div class="bullets-container <?php echo $classCol; ?>">
         <?php while (have_rows('bullets')): the_row(); ?>
+
+            <?php
+            $icon = get_sub_field('ikona');
+            $title = get_sub_field('tutul');
+            $description = get_sub_field('opis');
+            $color = get_sub_field('kolor');
+            $class = '';
+            if (!empty($color)) {
+                $class = "white-content";
+            }
+            ?>
             <div class="bullet-item" data-aos="fade-up">
-                <?php
-                $icon = get_sub_field('ikona');
-                $title = get_sub_field('tutul');
-                $description = get_sub_field('opis');
-                $color = get_sub_field('kolor');
-                ?>
                 <?php if ($icon): ?>
-                    <div class="bullet-icon">
+                    <div class="bullet-icon <?php echo $class; ?>">
                         <img src="<?php echo esc_url($icon['url']); ?>" alt="<?php echo esc_attr($icon['alt']); ?>">
                     </div>
                 <?php endif; ?>
 
                 <?php if ($title): ?>
-                    <p class="bullet-title"> <?php echo $title; ?> </p>
+                    <p class="bullet-title <?php echo $class; ?>"> <?php echo $title; ?> </p>
                 <?php endif; ?>
 
                 <?php if ($description): ?>
-                    <p class="bullet-description"> <?php echo esc_html($description); ?> </p>
+                    <p class="bullet-description <?php echo $class; ?>"> <?php echo esc_html($description); ?> </p>
                 <?php endif; ?>
                 <?php if ($color): ?>
                     <span class="bullet-color" style="background-color: <?php echo $color; ?>;"></span>
