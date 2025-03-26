2698a254fe82f4bc20ee4db44a69781dd0773cce
Creastv <creastv@gail.com>
2025-03-26 15:08:50 +0100

    fadein

--- blocks/bullets/bullets.php
+++ blocks/bullets/bullets.php
@@ -14,8 +14,8 @@ if ($col == 2) {
                 $icon = get_sub_field('ikona');
                 $title = get_sub_field('tutul');
                 $description = get_sub_field('opis');
+                $color = get_sub_field('kolor');
                 ?>
-
                 <?php if ($icon): ?>
                     <div class="bullet-icon">
                         <img src="<?php echo esc_url($icon['url']); ?>" alt="<?php echo esc_attr($icon['alt']); ?>">
@@ -29,6 +29,10 @@ if ($col == 2) {
                 <?php if ($description): ?>
                     <p class="bullet-description"> <?php echo esc_html($description); ?> </p>
                 <?php endif; ?>
+                <?php if ($color): ?>
+                    <span class="bullet-color" style="background-color: <?php echo $color; ?>;"></span>
+                <?php endif; ?>
+
             </div>
         <?php endwhile; ?>
     </div>
