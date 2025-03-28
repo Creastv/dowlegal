1aad5095aa90b6d793c1d34e27bfe80ec71269de
Creastv <creastv@gail.com>
2025-03-28 12:51:03 +0100

    sukcesy

--- page.php
+++ page.php
@@ -4,7 +4,7 @@ get_header();
 <?php
 while (have_posts()) : the_post(); ?>
 
-    <article id="post-<?php the_ID(); ?>" class="hentry page" data-aos="fade-up">
+    <article id="post-<?php the_ID(); ?>" class="hentry page">
         <?php get_template_part('templates-parts/header/header', 'title'); ?>
         <div class="entry-content">
             <?php the_content(); ?>
